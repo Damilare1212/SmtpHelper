639e499c11bd8d2522c2882057a292e94ce1b251
Michael Armani <michaelarmani00@gmail.com>
2021-12-08 15:03:40 +0100

    Sending mail test

--- SmtpHelperTests/SmtpHelperTests.cs
+++ SmtpHelperTests/SmtpHelperTests.cs
@@ -1,21 +1,40 @@
 using Xunit;
 using MA.SmtpHelper;
+using MA.SmtpHelper.Models;
 using Microsoft.Extensions.Configuration;
 
 namespace SmtpHelperTests;
 
 public class SmtpHelper
 {
+    private MailSenderConfiguration _mailSenderConfiguration;
+
     public SmtpHelper()
     {
         IConfigurationRoot? configuration = new ConfigurationBuilder()
             .AddUserSecrets<SmtpHelper>()
             .Build();
+
+        _mailSenderConfiguration = new MailSenderConfiguration()
+        {
+            Host = configuration["Smtp:Host"],
+            User = configuration["Smtp:User"],
+            Password = configuration["Smtp:Password"],
+            Port = 587,
+            UseSsl = false
+        };
     }
     
-    [Fact]
+    [Fact(DisplayName = "Simple send test")]
     public void TestSend()
     {
-        
+        MailSender
+            .Connect(_mailSenderConfiguration)
+            .From("test@test.com", "Test")
+            .SendTo("test1@test.com", "Test 1")
+            .Done()
+            .Subject("Test")
+            .Body("Test content", false)
+            .Send();
     }
 }
