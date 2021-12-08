72bb02e7482d3d3f168de1d845810780b28a3e9b
Michael Armani <michaelarmani00@gmail.com>
2021-12-08 14:57:12 +0100

    Add user settings for testings

--- SmtpHelperTests/SmtpHelperTests.cs
+++ SmtpHelperTests/SmtpHelperTests.cs
@@ -1,10 +1,18 @@
 using Xunit;
 using MA.SmtpHelper;
+using Microsoft.Extensions.Configuration;
 
 namespace SmtpHelperTests;
 
 public class SmtpHelper
 {
+    public SmtpHelper()
+    {
+        IConfigurationRoot? configuration = new ConfigurationBuilder()
+            .AddUserSecrets<SmtpHelper>()
+            .Build();
+    }
+    
     [Fact]
     public void TestSend()
     {
