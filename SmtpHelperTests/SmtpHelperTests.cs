46f15880d902f15990a658a9b8881b9d92a16609
Michael Armani <michaelarmani00@gmail.com>
2021-12-08 15:06:35 +0100

    Test HTML Content

--- SmtpHelperTests/SmtpHelperTests.cs
+++ SmtpHelperTests/SmtpHelperTests.cs
@@ -24,17 +24,31 @@ public class SmtpHelper
             UseSsl = false
         };
     }
+
+    private IMailContent GetMailHeader()
+    {
+        return MailSender
+            .Connect(_mailSenderConfiguration)
+            .From("test@test.com", "Test")
+            .SendTo("test1@test.com", "Test 1")
+            .Done();
+    }
     
     [Fact(DisplayName = "Simple send test")]
     public void TestSend()
     {
-        MailSender
-            .Connect(_mailSenderConfiguration)
-            .From("test@test.com", "Test")
-            .SendTo("test1@test.com", "Test 1")
-            .Done()
+        GetMailHeader()
             .Subject("Test")
             .Body("Test content", false)
+            .Send();
+    }
+    
+    [Fact(DisplayName = "Send Html body send test")]
+    public void TestHtmlSend()
+    {
+        GetMailHeader()
+            .Subject("Test HTML")
+            .Body("<h1>Test h1</h1> <strong>Test<strong>", true)
             .Send();
     }
 }
